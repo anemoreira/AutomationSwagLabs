fae17e0a0047c5443fb44f110273989025635b0b
Adejane Silva <adejanems@gmail.com>
2025-03-08 11:41:22 -0300

    config(cypress): adding environment settings

---
+++ cypress.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "cypress";
+import * as dotenv from "dotenv";
+import "cypress-mochawesome-reporter";
+
+dotenv.config();
+
+export default defineConfig({
+  chromeWebSecurity: false,
+  viewportWidth: 1024,
+  viewportHeight: 768,
+  video: true, 
+  screenshotOnRunFailure: true, 
+  e2e: {
+    setupNodeEvents(on, config) {
+
+      config.env = {
+        username: process.env.CYPRESS_username,
+        password: process.env.CYPRESS_password,
+        textFirstName: process.env.CYPRESS_TEXT_FIRST_NAME,
+        textLastName: process.env.CYPRESS_TEXT_LAST_NAME,
+        textPostalCode: process.env.CYPRESS_TEXT_POSTAL_CODE,    
+      };
+
+      return config;
+    },
+    baseUrl: "https://www.saucedemo.com/",
+    videosFolder: "cypress/videos",
+    screenshotsFolder: "cypress/screenshots",
+  },
+});
